54f5ed548ffd681101f8877a6ee850275fee9c8f
Azyleea <97592652+Azyleea@users.noreply.github.com>
2022-11-16 12:15:36 +0800

    Add files via upload

---
+++ src/android/PrivacyScreenPlugin.java
@@ -0,0 +1,66 @@
+/**
+ * PrivacyScreenPlugin.java Cordova Plugin Implementation
+ * Created by Tommy-Carlos Williams on 18/07/14.
+ * Copyright (c) 2014 Tommy-Carlos Williams. All rights reserved.
+ * MIT Licensed
+ */
+package org.devgeeks.privacyscreen;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
+
+import android.app.Activity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * This class sets the FLAG_SECURE flag on the window to make the app
+ *  private when shown in the task switcher
+ */
+//public class PrivacyScreenPlugin extends CordovaPlugin {
+//
+//  @Override
+//  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+//    super.initialize(cordova, webView);
+//    Activity activity = this.cordova.getActivity();
+//    activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+//  }
+//}
+
+public class FlagSecureTestActivity extends Activity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    getWindow().setFlags(LayoutParams.FLAG_SECURE,
+            LayoutParams.FLAG_SECURE);
+
+    setContentView(R.layout.main);
+  }
+}
+
+  override fun onPause() {
+
+  var parentView = findViewById<RelativeLayout>(R.id.parentView)
+          var splashScreen = layoutInflater.inflate(R.layout.splash_screen_custom, null)
+  parentView.addView(splashScreen, parentView.width, parentView.height)
+
+  super.onPause()
+
+}
+
+  override fun onResume() {
+  var parentView = findViewById<RelativeLayout>(R.id.parentView)
+          parentView.removeView(findViewById<RelativeLayout>(R.id.customSplash))
+  super.onResume()
+
+}
